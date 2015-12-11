ca6d46e229d78a632acc2cffda89cfbc6c69c11f
Oliver Cooper <oliver.cooper@me.com>
2015-12-12 00:08:28 +1030

    Updated TopBarSeparatorView to new class system

--- system/classes/TopBar/TopBarSeparatorView.lua
+++ system/classes/TopBar/TopBarSeparatorView.lua
@@ -1,14 +1,15 @@
 
 class "TopBarSeparatorView" extends "SeparatorView" {
     
-    activeSeparatorObject = false;
+    activeX = Number( 1 );
+    activeWidth = Number( 1 );
 
 }
 
 function TopBarSeparatorView:initialiseCanvas()
     self.super:initialiseCanvas()
     local theme, canvas = self.theme, self.canvas
-    local activeSeparatorObject = canvas:insert( Separator( 1, 1, 1, self.height ) )
+    local activeSeparatorObject = canvas:insert( Separator( self.activeX, 1, self.activeWidth, self.height ) )
 
     theme:connect( activeSeparatorObject, "fillColour", "activeSeparatorColour" )
     theme:connect( activeSeparatorObject, "isDashed", "activeSeparatorIsDashed" )
@@ -16,18 +17,12 @@ function TopBarSeparatorView:initialiseCanvas()
     self.activeSeparatorObject = activeSeparatorObject
 end
 
-function TopBarSeparatorView.activeX:set( x )
-   self.activeSeparatorObject.x = x 
+function TopBarSeparatorView.activeX:set( activeX )
+   self.activeX = activeX
+   self.needsDraw = true
 end
 
-function TopBarSeparatorView.activeWidth:set( width )
-    self.activeSeparatorObject.width = width 
-end
-
-function TopBarSeparatorView.activeX:get( x )
-    return self.activeSeparatorObject.x
-end
-
-function TopBarSeparatorView.activeWidth:get( width )
-    return self.activeSeparatorObject.width
+function TopBarSeparatorView.activeWidth:set( activeWidth )
+    self.activeWidth = activeWidth
+    self.needsDraw = true
 end
