afcd3f18c3abd9ab0328ce5ab2295d452469dcbe
Oliver Cooper <oliver.cooper@me.com>
2015-12-12 13:52:36 +1030

    Updated ProgramItem to new graphics system

--- system/classes/TopBar/ProgramItem.lua
+++ system/classes/TopBar/ProgramItem.lua
@@ -18,52 +18,21 @@ class "ProgramItem" extends "TopBarItem" {
 
 }
 
-function ProgramItem:initialiseCanvas()
+function ProgramItem:onDraw()
     self:super()
     local width, height, theme, canvas, program = self.width, self.height, self.theme, self.canvas, self.program
-    local titleObject = canvas:insert( Text( 1, 4, width, 8, program and program.title or "", Font.systemFont ) )
-    local statusObject = canvas:insert( Text( 1, 4, width, 8, program and program.status or "", Font.systemFont ) )
-    local iconObject = canvas:insert( Rectangle( 1, 3, ICON_WIDTH, ICON_HEIGHT, Graphics.colours.GREEN ) )
 
-    theme:connect( titleObject, "textColour", "titleColour" )
-    theme:connect( statusObject, "textColour", "statusColour" )
+    local leftMargin, rightMargin, topMargin, bottomMargin, iconTitleMargin, titleStatusMargin = theme:value( "leftMargin" ), theme:value( "rightMargin" ), theme:value( "topMargin" ), theme:value( "bottomMargin" ), theme:value( "iconTitleMargin" ), theme:value( "titleStatusMargin" )
+    local maxContentWidth = width - leftMargin - rightMargin
+
+    local titleFont, statusFont = theme:value( "titleFont" ), theme:value( "statusFont" )
+    local programTitle, programStatus = program.title, program.status or ""
+    local titleWidth, statusWidth = titleFont:getWidth( programTitle ), statusFont:getWidth( programStatus )
     
-    self.titleObject = titleObject
-    self.statusObject = statusObject
-    self.iconObject = iconObject
-end
-
-function ProgramItem:updateThemeStyle()
-    self.theme.style = self.isPressed and "pressed" or ( self.program.programView.isFocused and "focused" or "default" )
-end
-
-function ProgramItem.program:set( program )
-    self.program = program
-    local titleObject, statusObject = self.titleObject, self.statusObject
-    local title, status = program.title, program.status
-    titleObject.text = title
-    titleObject.width = titleObject.font:getWidth( title )
-
-    statusObject.text = status
-    statusObject.width = statusObject.font:getWidth( status )
-    self:updateThemeStyle()
-end
-
-function ProgramItem:updateWidth( width )
-    self:super( width )
-
-    local titleObject, iconObject, statusObject = self.titleObject, self.iconObject, self.statusObject
-    local titleWidth = titleObject.width
-    local statusWidth = statusObject.width
-    
-    local iconX, titleX, statusX
-
-    local maxContentWidth = width - 2 * MIN_MARGIN
-
     local configs = {
-        { ICON_WIDTH + ICON_TITLE_GAP + titleWidth + TITLE_STATUS_GAP + statusWidth, true, true, true };
-        { titleWidth + TITLE_STATUS_GAP + statusWidth, false, true, true };
-        { ICON_WIDTH + ICON_TITLE_GAP + titleWidth, true, true, false };
+        { ICON_WIDTH + iconTitleMargin + titleWidth + titleStatusMargin + statusWidth, true, true, true };
+        { titleWidth + titleStatusMargin + statusWidth, false, true, true };
+        { ICON_WIDTH + iconTitleMargin + titleWidth, true, true, false };
         { titleWidth, false, true, false };
         { ICON_WIDTH, true, false, false };
     }
@@ -80,30 +49,31 @@ function ProgramItem:updateWidth( width )
     local x = math.floor( ( width - selectedConfig[1] ) / 2 ) + 1
     -- Icon
     if selectedConfig[2] then
-        iconObject.x = x
-        x = x + ICON_WIDTH + (selectedConfig[3] and ICON_TITLE_GAP or 0)
-        iconObject.isVisible = true
-    else
-        iconObject.isVisible = false
+        local iconMask = RectangleMask( x, 1 + topMargin, ICON_WIDTH, ICON_HEIGHT )
+        canvs:fill( Graphics.colours.GREEN, iconMask )
+        x = x + ICON_WIDTH + (selectedConfig[3] and iconTitleMargin or 0)
     end
 
     -- Title
     if selectedConfig[3] then
-        titleObject.x = x
-        x = x + titleWidth + (selectedConfig[4] and TITLE_STATUS_GAP or 0)
-        titleObject.isVisible = true
-    else
-        titleObject.isVisible = false
+        canvas:fill( theme:value( "titleColour" ), TextMask( x, 1 + topMargin, titleWidth, titleFont.height, programTitle, titleFont ) )
+        x = x + titleWidth + (selectedConfig[4] and titleStatusMargin or 0)
     end
 
     -- Status
     if selectedConfig[4] then
-        statusObject.x = x
-        statusObject.isVisible = true
-    else
-        statusObject.isVisible = false
+        canvas:fill( theme:value( "statusColour" ), TextMask( x, 1 + topMargin, statusWidth, statusFont.height, programStatus, statusFont ) )
     end
+end
 
+function ProgramItem:updateThemeStyle()
+    self.theme.style = self.isPressed and "pressed" or ( self.program.programView.isFocused and "focused" or "default" )
+end
+
+function ProgramItem.program:set( program )
+    self.program = program
+    self.needsDraw = true
+    self:updateThemeStyle()
 end
 
 function ProgramItem:onGlobalMouseUp( MouseUpEvent event, Event.phases phase )
