a4c21e70a3ee15f04cf9e88e0915038e68cbb3ee
Oliver Cooper <oliver.cooper@me.com>
2015-12-12 13:52:02 +1030

    Updated HomeItem to new graphics system

--- system/classes/TopBar/HomeItem.lua
+++ system/classes/TopBar/HomeItem.lua
@@ -2,7 +2,7 @@
 class "HomeItem" extends "TopBarItem" {
     
     pin = Number( TopBarItem.pins.LEFT );
-
+    homeContainer = HomeContainer.allowsNil;
     size = Number( 29 ).allowsNil;
 
 }
@@ -17,65 +17,12 @@ function HomeItem:onReady( ReadyInterfaceEvent event, Event.phases phase )
     self.homeContainer = self.application.container.homeContainer
 end
 
-function HomeItem:initialiseCanvas()
-    log("home item initialiseCanvas")
-    log(tostring(self))
-    self:super()
-    local width, height, theme, canvas = self.width, self.height, self.theme
-
-    -- TODO: !URGENT! path drawing bug
-
-
-    -- local homeObject = Path( 10, 2, 9, 9, 5, 1 )
-    -- homeObject:lineTo( 9, 5 )
-    -- homeObject:lineTo( 8, 5 )
-    -- homeObject:lineTo( 8, 9 )
-    -- homeObject:lineTo( 6, 9 )
-    -- homeObject:lineTo( 6, 7 )
-    -- homeObject:lineTo( 4, 7 )
-    -- homeObject:lineTo( 4, 9 )
-    -- homeObject:lineTo( 2, 9 )
-    -- homeObject:lineTo( 2, 5 )
-    -- homeObject:lineTo( 1, 5 )
-    -- homeObject:close()
-
-    -- self.canvas:insert( homeObject )
-
-    -- theme:connect( homeObject, "fillColour", "contentColour" )
-    
-    -- self.homeObject = homeObject
-
-
-
-    -- local homeRoofObject = Path( 11, 3, 9, 9, 5, 1 )
-    -- homeRoofObject:lineTo( 9, 5 )
-    -- homeRoofObject:lineTo( 1, 5 )
-    -- homeRoofObject:close()
-
-    -- log(textutils.serialise(homeRoofObject:getSerialisedPath()))
-
-    -- local homeBodyObject = Path( 11, 3, 9, 9, 8, 5 )
-    -- homeBodyObject:lineTo( 8, 9 )
-    -- homeBodyObject:lineTo( 6, 9 )
-    -- homeBodyObject:lineTo( 6, 9 )
-    -- homeBodyObject:lineTo( 6, 7 )
-    -- homeBodyObject:lineTo( 4, 7 )
-    -- homeBodyObject:lineTo( 4, 9 )
-    -- homeBodyObject:lineTo( 2, 9 )
-    -- homeBodyObject:lineTo( 2, 5 )
-    -- homeBodyObject:close()
-    -- log(textutils.serialise(homeBodyObject:getSerialisedPath()))
-
-
-    local symbolObject = SymbolObject( 11, 3, HomeSymbol )
-    -- self.canvas:insert( homeRoofObject )
-    self.canvas:insert( symbolObject )
-
-    theme:connect( symbolObject, "fillColour", "contentColour" )
-    -- theme:connect( homeBodyObject, "fillColour", "contentColour" )
-    
-    self.symbolObject = symbolObject
-
+function HomeItem:onDraw()
+    local canvas, theme = self.canvas, self.theme
+    local symbol = theme:value( "symbol" )
+    local leftMargin, topMargin = theme:value( "leftMargin" ), theme:value( "topMargin" )
+    canvas:fill( theme:value( "fillColour" ) )
+    canvas:fill( theme:value( "contentColour" ), SymbolMask( 1 + leftMargin, 1 + topMargin, symbol ) )
 end
 
 function HomeItem:onAction( ActionInterfaceEvent event, Event.phases phase )
