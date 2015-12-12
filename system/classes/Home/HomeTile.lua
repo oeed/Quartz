5ecb19bace5b553942ada2fec9975cfd08d02c2e
Oliver Cooper <oliver.cooper@me.com>
2015-12-12 13:51:33 +1030

    Updated HomeTile to new graphics system

--- system/classes/Home/HomeTile.lua
+++ system/classes/Home/HomeTile.lua
@@ -3,27 +3,18 @@ local SHADOW_SIZE_X = 2
 local SHADOW_SIZE_Y = 3
 
 class "HomeTile" extends "Container" implements "IHomeItem" {
-    
-    backgroundObject = false;
-    shadowObject = false;
 
 }
 
-
-function HomeTile:initialiseCanvas()
+function HomeTile:onDraw()
     self:super()
     local width, height, theme, canvas = self.width, self.height, self.theme, self.canvas
-    local shadowObject = canvas:insert( RoundedRectangle( 1 + SHADOW_SIZE_X, 1 + SHADOW_SIZE_Y, width - SHADOW_SIZE_X, height - SHADOW_SIZE_Y, theme.shadowColour ) )
-    local backgroundObject = canvas:insert( RoundedRectangle( 1, 1, width - SHADOW_SIZE_X, height - SHADOW_SIZE_Y, theme.fillColour, theme.outlineColour, cornerRadius ) )
 
-    theme:connect( backgroundObject, "fillColour" )
-    theme:connect( backgroundObject, "outlineColour" )
-    theme:connect( backgroundObject, "radius", "cornerRadius" )
-    theme:connect( shadowObject, "fillColour", "shadowColour" )
-    theme:connect( shadowObject, "radius", "cornerRadius" )
+    local roundedRectangle = RoundedRectangleMask( 1, 1, width, height, theme:value( "cornerRadius" ) )
+    canvas:fill( theme:value( "fillColour" ), roundedRectangle )
+    canvas:outline( theme:value( "outlineColour" ), roundedRectangle, theme:value( "outlineThickness" ) )
 
-    self.backgroundObject = backgroundObject
-    self.shadowObject = shadowObject
+    self.shadowSize = theme:value( "shadowSize" )
 end
 
 function HomeTile:updateWidth( width )
