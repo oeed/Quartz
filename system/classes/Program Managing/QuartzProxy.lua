76e9e6050252ddf1c7e9cec85aefb57584de0a9f
Oliver Cooper <oliver.cooper@me.com>
2015-12-17 17:45:36 +1030

    Changed :redraw to accept the pixels table

--- system/classes/Program Managing/QuartzProxy.lua
+++ system/classes/Program Managing/QuartzProxy.lua
@@ -2,6 +2,7 @@
 class "QuartzProxy" {
 
     screenBuffer = Table.allowsNil;
+    silicaApplication = Table.allowsNil; -- we have to use table because the Application class will be different, we just have to presume it's an Application
     _program = Program;
     
 }
@@ -10,6 +11,10 @@ function QuartzProxy:initialise( Program program )
     self._program = program
 end
 
-function QuartzProxy:redraw()
-    self._program.programView.needsDraw = true
+function QuartzProxy:redraw( Table.allowsNil pixels )
+    local programView = self._program.programView
+    if pixels then
+        programView.buffer = pixels
+    end
+    programView.needsDraw = true
 end
