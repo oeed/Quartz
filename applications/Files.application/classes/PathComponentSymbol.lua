2b2684f07d36d2c07e9d6fbcc1fd989a20c6a02e
Oliver Cooper <oliver.cooper@me.com>
2015-12-29 20:51:07 +1300

    Added component drawing to ToolbarPathTextBox

---
+++ applications/Files.application/classes/PathComponentSymbol.lua
@@ -0,0 +1,19 @@
+
+class "PathComponentSymbol" extends "Symbol" {
+
+    static = {
+        symbolName = String( "pathComponent" );
+        width = Number( 3 );
+        height = Number( 5 );
+    };
+
+}
+
+function PathComponentSymbol.static:initialise()
+    local path = Path( self.width, self.height, 1, 1 )
+    path:lineTo( 3, 3 )
+    path:lineTo( 1, 5 )
+    path:close()
+    
+    self:super( path )
+end
--- applications/Files.application/classes/ToolbarPathTextBox.lua
+++ applications/Files.application/classes/ToolbarPathTextBox.lua
@@ -29,40 +29,62 @@ function ToolbarPathTextBox.path:set( path )
         self.path = path
 
         local pathComponents = {}
+        local theme = self.theme
         local font, leftMargin, componentLeftMargin, componentRightMargin = theme:value( "componentFont" ), theme:value( "leftMargin" ), theme:value( "componentLeftMargin" ), theme:value( "componentRightMargin" )
-        local x = leftMargin
         for text in path:gmatch( "/([^/]*)" ) do
-            local width = componentLeftMargin + font:getWidth( text ) + componentRightMargin,
+            local width = componentLeftMargin + font:getWidth( text ) + componentRightMargin
             table.insert( pathComponents, {
-                x = x;
                 width = math.min( width, MAX_COMPONENT_WIDTH );
                 text = text;
             } )
-            x = x + width
         end
+        self.pathComponents = pathComponents
     end
 end
 
 function ToolbarPathTextBox:onDraw()
     self:super()
     local isFocused = self.isFocused
-    if isFocused then
+    if not isFocused then
         local width, height, theme, canvas, path, pathComponents = self.width, self.height, self.theme, self.canvas, self.path, self.pathComponents
-    
-        local leftMargin, rightMargin, topMargin, bottomMargin, componentLeftMargin, componentRightMargin = theme:value( "leftMargin" ), theme:value( "rightMargin" ), theme:value( "topMargin" ), theme:value( "bottomMargin" ), theme:value( "componentLeftMargin" ), theme:value( "componentRightMargin" )
+        local font, componentLeftMargin, componentRightMargin, componentFirstLeftMargin, leftMargin, rightMargin, topMargin, bottomMargin = theme:value( "componentFont" ), theme:value( "componentLeftMargin" ), theme:value( "componentRightMargin" ), theme:value( "componentFirstLeftMargin" ), theme:value( "leftMargin" ), theme:value( "rightMargin" ), theme:value( "topMargin" ), theme:value( "bottomMargin" )
 
         -- if there are too many components we'll chop out the first few until there is room
         local totalWidth = leftMargin + rightMargin
         local firstFullComponent = 1
-        for i = #pathComponents, 1, -1 do
-            local component = components[i]
-            totalWidth = totalWidth + component.width
-            if totalWidth > width then
-                firstFullComponent = i + 1
+        local symbol = theme:value( "componentSymbol" )
+        local symbolWidth, symbolHeight = symbol.width, symbol.height
+        local ellipsisWidth = componentLeftMargin + font:getWidth( "..." ) + componentRightMargin
+        local pathComponentsCount = #pathComponents
+        for i = pathComponentsCount, 1, -1 do
+            local component = pathComponents[i]
+            totalWidth = totalWidth + component.width + ( i ~= pathComponentsCount and symbolWidth or 0 )
+            if totalWidth + ellipsisWidth > width then
+            firstFullComponent = i + 1
             end
         end
 
-        -- local pathComponents = path:match( "/([^/]*)" )
-        -- log(textutils.serialize(pathComponents))
+        local componentTextColour = theme:value( "componentTextColour" )
+        local componentSymbolColour = theme:value( "componentSymbolColour" )
+        local x = 1 + leftMargin - componentLeftMargin
+        local symbolY = math.ceil( ( height - symbolHeight ) / 2 ) + 1
+        for i = 1, pathComponentsCount do
+            local text, width
+            if firstFullComponent == i + 1 then
+                text = "..."
+                width = ellipsisWidth
+            elseif firstFullComponent <= i then
+                local component = pathComponents[i]
+                text = component.text
+                width = component.width
+            end
+            if text then
+                canvas:fill( componentTextColour, TextMask( x + componentLeftMargin, 1 + topMargin, width - componentLeftMargin - componentRightMargin, height - topMargin - bottomMargin, text, font ) )
+                if i ~= pathComponentsCount then
+                    canvas:fill( theme:value( "componentSymbolColour" ), SymbolMask( x + width, symbolY, symbol ) )
+                end
+                x = x + width + symbolWidth
+            end
+        end
     end
 end
