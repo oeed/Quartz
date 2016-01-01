19469ade5661583b48bc80fff304c2440c8490c4
Oliver Cooper <oliver.cooper@me.com>
2016-01-01 18:09:30 +1300

    Added path setting to FilesApplicationContainer

--- applications/Files.application/classes/FilesApplicationContainer.lua
+++ applications/Files.application/classes/FilesApplicationContainer.lua
@@ -2,4 +2,28 @@
 class "FilesApplicationContainer" extends "ApplicationContainer" {
     
     height = Number( 186 );
+    path = String.allowsNil;
+    pathTextBox = ToolbarPathTextBox.link;
+
 }
+
+function FilesApplicationContainer:initialise( ... )
+    self:super( ... )
+
+   self:event( ReadyInterfaceEvent, self.onReady )
+end
+
+function FilesApplicationContainer:onReady( ReadyInterfaceEvent event, Event.phases phase )
+    self.path = self.application.arguments[1] or "/user"
+end
+
+function FilesApplicationContainer.path:set( path )
+    if path:sub( 1, 1) ~= "/" then
+        path = "/" .. path
+    end
+    path = FileSystemItem.static:resolve( path )
+    if FileSystemItem.static:exists( path ) then
+        self.path = path
+        self.pathTextBox.path = path
+    end
+end
